b24309c1a825c056a77153bf9832db5450976d7a
Bon-Minh Lam <bmlam@sefrowo.com>
2017-05-13 19:35:33 +0200

    did list errMsg,srcLine,srcPin

--- LsCompileIssues.swift
+++ LsCompileIssues.swift
@@ -62,10 +62,41 @@ class Helper {
 	   }
 		return lines
 	}
+
+	static func printIfNotNull( _ str: String? ) {
+		if str != nil && str!.characters.count > 0 {
+			print( str! )
+		}
+	}
+
 } //Helper
 
 //MARK: CompileIssue 
+
 class CompileIssue {
+	/* examples of issues: 
+	
+	Type 1 (3 lines msg,sourceLine,pointer):
+	
+		/Users/.../ViewController.swift:50:4: error: expected '{' to start the body of for-each loop
+	                        self.mapView.addAnnotation( pin )
+	                        ^
+	Type 2 (5 lines msg,sourceLine,pointer,note,referencedLine,pointer):
+	
+	/Users/.../RootViewController.swift:49:14: error: use of unresolved identifier 'mountainPin'
+	              		for pin in mountainPin//s {
+	                           ^~~~~~~~~~~
+	/Users/.../Global.swift:4:5: note: did you mean 'mountainPins'?
+	var mountainPins = [MyPin]()
+	    ^
+	
+	For type 1: we store sourceLine and pointer as one property
+	For type 2: we store the note,referencedLine,pointer as one property
+	
+	*/
+
+	enum Types { case error, warning }
+
 	enum FsmStates {
 		case initial
 		, srcLineExpected, srcPinExpected
@@ -86,73 +117,78 @@ class CompileIssue {
 					fatalError( "Found more than 1 match for '\(regexEngine.pattern)' in following line!\n\(string)") 
 			}
 		}
-		return cntMatch <= 1
+		let rc = cntMatch == 1
+		//_dbx( "rc: \(rc)" )
+		return rc
 	}
 
-	public var errMsg = String()
+	var type = CompileIssue.Types.error
+
+	public var mainMsg = String()
 	public var srcLine = String()
 	public var srcPin = String()
 	public var noteMsg = String()
 	public var refLine = String()
 	public var refPin = String()
 
-	init( errMsg: String ) { self.errMsg = errMsg }
+	init( errMsg: String ) { self.mainMsg = errMsg }
+	init( warnMsg: String ) { 
+		self.mainMsg = warnMsg 
+		self.type = CompileIssue.Types.warning
+	}
 
 }
 
-let (inputPath, listWarnings) = Helper.getSettings()
-_dbx( "inputPath: \(inputPath)" )
-_dbx("listWarnings: \(listWarnings)")
-let lines = Helper.readLinesFromFile ( path: inputPath )
-
-
-var fsmState = CompileIssue.FsmStates.initial 
-for (lno, lnText) in lines.enumerated() {
-	_dbx("lno: \(lno)")
-	//for debugging print line begin
-	let indexMonkey /*bcos the index concept is monkey-like*/ = lnText.characters.count > 80 
-			? lnText.index( lnText.startIndex, offsetBy: 80 ) 
-			: lnText.index( lnText.startIndex, offsetBy: lnText.characters.count ) 
-	let textChunk = lnText.substring( to: indexMonkey )
-	_dbx("textChunk: \(textChunk)")
-
-/* examples of issues: 
-
-Type 1 (3 lines msg,sourceLine,pointer):
-
-	/Users/bmlam/Dropbox/my-apps/MountainsAround/MountainsAround/RootViewController.swift:50:4: error: expected '{' to start the body of for-each loop
-                        self.mapView.addAnnotation( pin )
-                        ^
-Type 2 (5 lines msg,sourceLine,pointer,note,referencedLine,pointer):
-
-/Users/bmlam/Dropbox/my-apps/MountainsAround/MountainsAround/RootViewController.swift:49:14: error: use of unresolved identifier 'mountainPin'
-                for pin in mountainPin//s {
-                           ^~~~~~~~~~~
-	/Users/bmlam/Dropbox/my-apps/MountainsAround/MountainsAround/Global.swift:4:5: note: did you mean 'mountainPins'?
-var mountainPins = [MyPin]()
-    ^
-
-For type 1: we store sourceLine and pointer as one property
-For type 2: we store the note,referencedLine,pointer as one property
-
-*/
+func main() {
+	let (inputPath, listWarnings) = Helper.getSettings()
+	// _dbx( "inputPath: \(inputPath)" )
+	_dbx("listWarnings: \(listWarnings)")
+	let lines = Helper.readLinesFromFile ( path: inputPath )
 	var issues = [CompileIssue]()
-	switch fsmState {
-		case .initial :
-			if CompileIssue.doesMatchAtMostOnce( regexEngine: CompileIssue.errMsgRegex, string: lnText ) {
-				fsmState = CompileIssue.FsmStates.srcLineExpected
-				issues.append( CompileIssue( errMsg: lnText ) )
-			}
-		case .srcLineExpected:		_dbx("line: \(#line)")
-		case .srcPinExpected:		_dbx("line: \(#line)")
-		case .noteMsgExpected: 		_dbx("line: \(#line)")
-		case .refLineExpected: 		_dbx("line: \(#line)")
-		case .refPinExpected:		_dbx("line: \(#line)")
-
-	} //switch
-
+	
+	var fsmState = CompileIssue.FsmStates.initial 
+	for (lno, lnText) in lines.enumerated() {
+		//for debugging print line begin
+		let indexMonkey /*bcos the index concept is monkey-like*/ = lnText.characters.count > 80 
+				? lnText.index( lnText.startIndex, offsetBy: 80 ) 
+				: lnText.index( lnText.startIndex, offsetBy: lnText.characters.count ) 
+		let textChunk = lnText.substring( to: indexMonkey )
+		_dbx("textChunk: \(textChunk)")
+	
+		_dbx("fsmState: \(fsmState)")
+		switch fsmState {
+			case .initial :
+				if CompileIssue.doesMatchAtMostOnce( regexEngine: CompileIssue.errMsgRegex, string: lnText ) {
+					fsmState = CompileIssue.FsmStates.srcLineExpected
+					issues.append( CompileIssue( errMsg: lnText ) )
+				}
+			case .srcLineExpected:		
+				issues.last!.srcLine = lnText
+				fsmState = CompileIssue.FsmStates.srcPinExpected
+			case .srcPinExpected:	
+				issues.last!.srcPin = lnText
+				fsmState = CompileIssue.FsmStates.noteMsgExpected
+			case .noteMsgExpected: 		
+				fsmState = CompileIssue.FsmStates.initial
+//			case .refLineExpected: 		_dbx("line: \(#line)")
+//			case .refPinExpected:		_dbx("line: \(#line)")
+			default: break
+	
+		} //switch
+	
+		// if lno > 12 { _dbx( "TEST: reached line \(lno)"); break }
+	}
 	_dbx("issues.count: \(issues.count)")
+	for (ix, issue) in issues.enumerated() where issue.type == .error {
+		_dbx( "----  issue no. \(ix)" )
+		print( issue.mainMsg )
+		print( issue.srcLine )
+		print( issue.srcPin )
+		Helper.printIfNotNull( issue.noteMsg )
+	}
 }
+
+main()
 
 // print("xyz: \(xyz)")
 // _dbx("xyz: \(xyz)")
